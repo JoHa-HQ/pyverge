d2a541867eaeab6a691c143485c27440b4cc7cd1
TheLazzzies <mxyakovenko9@gmail.com>
2026-07-11 10:59:48 +0300

    feat(#12): add utils for tests. updated the tests for version property

---
+++ tests/utils.py
@@ -0,0 +1,23 @@
+from typing import cast
+
+import pendulum
+from semver import Version
+
+from pydantic_migrator.migration import (
+    MigrationSettings,
+    TypeInspector,
+    VersionedModel,
+    types,
+)
+
+
+def envelope_model(
+    migration_settings: MigrationSettings, model_cls: type[types.VModel]
+) -> VersionedModel[types.VersionValue, types.VModel]:
+    version = TypeInspector.get_literal_values(
+        model_cls.model_fields[migration_settings.version_property].annotation
+    )
+    parsed_version = VersionedModel.of(cast(str, version))
+    return VersionedModel[types.VersionValue, model_cls](
+        _value=cast(types.VersionValue, parsed_version), _model_cls=model_cls
+    )
